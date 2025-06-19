7e33703c8f04e9f5a934c9c8d2127a6f06d3b471
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-06-19 23:57:21 +0200

    consider disqualifications when sending notifications

--- .github/scripts/should-send-notif.sh
+++ .github/scripts/should-send-notif.sh
@@ -8,9 +8,10 @@ if [ ! -f "summary.txt" ]; then
 fi
 
 prs_merged=$(grep 'PRs merged' summary.txt | cut -d: -f2 | xargs || echo 0)
+prs_disqualified=$(grep 'PRs disqualified' summary.txt | cut -d: -f2 | xargs || echo 0)
 errors_encountered=$(grep 'Errors encountered' summary.txt | cut -d: -f2 | xargs || echo 0)
 
-if [ "$prs_merged" = "0" ] && [ "$errors_encountered" = "0" ]; then
+if [ "$prs_merged" = "0" ] && [ "$prs_disqualified" = "0" ] && [ "$errors_encountered" = "0" ]; then
     echo -n "0"
 else
     echo -n "1"
